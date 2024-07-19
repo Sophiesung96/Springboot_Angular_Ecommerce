e309f9a63f8f8649fa30df4ef6983b59bc563675
ssun0084 <ssun0084@student.monash.edu>
2024-07-19 10:36:39 +1000

    Second commit

---
+++ src/test/java/com/example/springbootangularecommerce/SpringbootAngularEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootangularecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
