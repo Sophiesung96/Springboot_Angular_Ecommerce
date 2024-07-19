4b4f695c856beb4a742619efa917acca3d795431
ssun0084 <ssun0084@student.monash.edu>
2024-07-19 19:38:06 +1000

    added CrossOrigin annotations

--- src/main/java/com/example/springbootangularecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/example/springbootangularecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,12 @@ package com.example.springbootangularecommerce.config;
 
 import com.example.springbootangularecommerce.entitiy.Product;
 import com.example.springbootangularecommerce.entitiy.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
