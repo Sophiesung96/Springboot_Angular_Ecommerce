495f61c3e9f1f3d5be26d292283797daa3fdcb9b
Sophiesung96 <tu6010534@gmail.com>
2024-11-27 17:03:14 +1100

    comment on the usage of @ManyToOne and @JoinColumn

--- src/main/java/com/example/springbootangularecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/example/springbootangularecommerce/dao/ProductCategoryRepository.java
@@ -1,6 +1,6 @@
 package com.example.springbootangularecommerce.dao;
 
-import com.example.springbootangularecommerce.entitiy.ProductCategory;
+import com.example.springbootangularecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
